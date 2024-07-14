bfd66517e8033d3484beda2ee7339e9a8ccf48c4
Achmad Ramadhani <gunspatriot02@gmail.com>
2024-07-14 13:11:22 +0800

    tugas mobile 2 dhana

---
+++ tugas_dhani/android/app/src/main/kotlin/com/example/tugas_dhani/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas_dhani
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tugas_dhani/lib/view/tabBar_login.dart
@@ -0,0 +1,45 @@
+
+import 'package:tugas_dhani/view/daftar_akun.dart';
+import 'package:tugas_dhani/view/login.dart';
+import 'package:flutter/material.dart';
+
+class TabBarLogin extends StatefulWidget {
+  const TabBarLogin({super.key});
+
+  @override
+  State<TabBarLogin> createState() => _TabBarLoginState();
+}
+
+class _TabBarLoginState extends State<TabBarLogin> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: 10,
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                text: "Login",
+              ),
+              Tab(
+                text: "Daftar",
+              ),
+            ],
+            labelStyle: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            labelColor: Color.fromARGB(255, 103, 121, 179),
+            indicatorColor: Color.fromARGB(255, 74, 76, 203),
+            dividerColor: Color.fromARGB(255, 73, 81, 166),
+          ),
+        ),
+        body: const TabBarView(children: [
+          Login(),
+          DaftarAkun(),
+        ]),
+      ),
+    );
+  }
+}
